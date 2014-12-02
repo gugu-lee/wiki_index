d8331521f343593c9b7c0812fcb46649fd5a1caf
gugu-lee <seven.stone.2012@gmail.com>
2014-12-03 01:18:35 +0800

    update output file

--- perl/output_index.pl
+++ perl/output_index.pl
@@ -9,7 +9,7 @@ my $dbh;
 my $fh;
 
 my $table_name="page_index";
-
+my $pre_row = undef;
 sub decode_url
 {
 	my $urldecode = uri_unescape($_[0]); 
@@ -30,8 +30,6 @@ sub open_db
 	my $pwd = "1234";
 	my $connection="dbi:mysql:$db:$host";
 	$dbh = DBI->connect($connection,$user,$pwd) or die "failed to connect mysql,$!";
-
-
 }
 sub close_db
 {
@@ -50,7 +48,7 @@ sub fetch_undeal
 	$sth->execute() or die "error:" . $sth->errstr;
 
 	while (@data = $sth->fetchrow()) {
-		push @ret,{'index_id'=>$data[0],'title'=>$data[2],'sub_category_count'=>$data[4],'page_count'=>$data[5],'path'=>$data[7],'level'=>$data[8]};
+		push @ret,{'index_id'=>$data[0],'title'=>$data[2],'sub_category_count'=>$data[4],'page_count'=>$data[5],'path'=>$data[7],'level'=>$data[8],'rel_id'=>$data[9]};
 	}
 	$sth->finish();
 	
@@ -58,7 +56,7 @@ sub fetch_undeal
 }
 
 
-sub output2
+sub output
 {
 	my ($offset,$page_size)=@_;
 	my @undeal = fetch_undeal($offset,$page_size);
@@ -68,21 +66,31 @@ sub output2
 	}
 	for my $row (@undeal) {
 
-		my $page = decode_url($row->{title});
+		my $title = decode_url($row->{title});
 		
-		my $level = $row->{level};
-		print $fh "<span style=\"padding-left:";
-		print $fh $level*20;
-
-		while ($level-- >0) {
-			#print "--";
+		if ($row->{sub_category_count}>0 and $row->{rel_id}==0) {
+			print $fh "<ul><li><input type=\"checkbox\" id=\"item-".$row->{index_id}."\" checked=\"checked\" /><label for=\"item-".$row->{index_id}."\">".$title."(sub-catagory:".$row->{sub_category_count}.",page:".$row->{page_count}.")</label>";
+		}else{
+			print $fh "<li><a href=\"./\">".$title."(sub-catagory:".$row->{sub_category_count}.",page:".$row->{page_count}.")</a></li>";
 		}
-		print $fh "px\">";
-		print $fh $page;
-		print $fh "(sub-catagory:".$row->{sub_category_count}.",page:".$row->{page_count}.")</span><br>\n";
-
-		#print $page."\n";
+		if ($pre_row) {
+			if ($row->{level}<$pre_row->{level}) {
+				print $fh "</ul>\n";
+			}
+		}
+		$pre_row=$row;
+		#
 		
+		#my $level = $row->{level};
+		#print $fh "<span style=\"padding-left:";
+		#print $fh $level*20;
+
+		#while ($level-- >0) {
+			#print "--";
+		#}
+		#print $fh "px\">";
+		#print $fh $title;
+		#print $fh "(sub-catagory:".$row->{sub_category_count}.",page:".$row->{page_count}.")</span><br>\n";		
 	}
 	return 1;
 }
@@ -91,18 +99,18 @@ sub main
 {
 	open_db();
 	open($fh, '>', "Index_C1.html") or die "Can't redirect STDOUT: $!";
-	print $fh "<!DOCTYPE HTML>\b<HTML lang=\"zh_CN\"><HEAD><meta charset=\"GBK\"> <TITLE> Index </TITLE> </HEAD> <BODY> <p><font color=\"blue\" size=\"4\">\n";
+	print $fh "<!DOCTYPE HTML><HTML lang=\"zh_CN\"><HEAD><meta charset=\"GBK\"> <TITLE> Index </TITLE> </HEAD> <BODY> <p><font color=\"blue\" size=\"4\">\n<div class=\"css-treeview\">\n<ul>";
 	
 	my $i=0;
 	my $page_size=10000;
 
 	while (1) {
-		if (!output2($i++ * $page_size,$page_size)){
+		if (!output($i++ * $page_size,$page_size)){
 			last;
 		}
 	}
 	
-	print $fh "\n</font></P> </BODY></HTML>\n";
+	print $fh "</ul>\n</div>\n</font></P> </BODY></HTML>\n";
 	close($fh);
 	
 	close_db();
